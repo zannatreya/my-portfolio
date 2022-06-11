37c1d6c25e36572256c17a0fbbac3d3c537be307
zannatreya <zannatulbintabahar@yahoo.com>
2022-06-12 01:12:46 +0600

    footer implemented

--- src/PortfolioContainer/Footer.js
+++ src/PortfolioContainer/Footer.js
@@ -1,19 +1,20 @@
-import React from "react"
-// import logo1 from "./pic/f_logo.png"
-
-const Footer = () => {
+import React from "react";
+import footerImg from "../assets/logo (1).png";
+function Footer() {
   return (
-    <>
-      <footer>
-        <div className='conatiner text-center top'>
-          <div className='img'>
-            {/* <img src={logo1} alt='' /> */}
-          </div>
-          <p>© 2021. All rights reserved by Zannatul Binta Bahar.</p>
-        </div>
-      </footer>
-    </>
-  )
+    <div
+      className="footer container mx-auto d-flex align-items-center justify-content-space-between pz-10"
+      style={{ padding: "10px 20px", zIndex: "100" }}
+    >
+      <img src={footerImg} alt="" className="footer__img pointer" />
+      <span
+        className="copyright"
+        style={{ color: "black", fontSize: "14px", opacity: "0.75", margin:"auto" }}
+      >
+        Copyright © 2022 Zannatul Binta Bahar. All Rights Reserved.
+      </span>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
